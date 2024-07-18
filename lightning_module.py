ba8f59bd711962464baada324984a7ae32b34730
pranzalkhadka <pranzalkhadka1@gmail.com>
2024-07-18 14:22:59 +0545

    lighning data module

--- lightning_module.py
+++ lightning_module.py
@@ -1,5 +1,4 @@
 import pytorch_lightning as pl
-# print(pl.__version__)
 from torch import nn, optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split
@@ -7,7 +6,6 @@ from torchvision import datasets, transforms
 import torch
 import torchmetrics
 from torchmetrics import Metric
-# print(torch.cuda.is_available())
 
 
 # class NN(nn.Module):
@@ -20,6 +18,37 @@ from torchmetrics import Metric
 #         x = F.relu(self.fc1(x))
 #         x = self.fc2(x)
 #         return x
+
+
+# entire_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+
+# train_ds, val_ds = random_split(entire_dataset, [55000, 5000])
+
+# test_ds = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+
+# train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+# val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=True)
+# test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
+# for epoch in range(num_epochs):
+#     for batch_idx, (data, targets) in enumerate(train_loader):
+#         data = data.to(device=device)
+#         targets = targets.to(device=device)
+
+#         data = data.reshape(data.shape[0], -1)
+
+#         scores = model(data)
+#         loss = criterion(scores, targets)
+
+#         optimizer.zero_grad()
+#         loss.backward()
+
+#         optimizer.step()
+
 
 
 class MyAccuracy(Metric):
@@ -90,6 +119,55 @@ class NN(pl.LightningModule):
         preds = torch.argmax(scores, dim=1)
         return preds
 
+class MnistDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir, batch_size, num_workers):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+        # Single GPU
+        datasets.MNIST(self.data_dir, train=True, download=True)
+        datasets.MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        # Multiple GPU
+        entire_dataset = datasets.MNIST(root= self.data_dir, train=True, transform=transforms.ToTensor(), download=False)
+        self.train_ds, self.val_ds = random_split(entire_dataset, [55000, 5000])
+
+        self.test_ds = datasets.MNIST(root=self.data_dir, train=False, transform=transforms.ToTensor(), download=False)
+
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+
+    def prepare_data(self):
+        datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+        datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+
+    def setup(self, stage=None):
+        if stage == 'fit' or stage is None:
+            entire_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+            self.train_ds, self.val_ds = random_split(entire_dataset, [55000, 5000])
+        if stage == 'test' or stage is None:
+            self.test_ds = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=self.batch_size, shuffle=True)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, batch_size=self.batch_size, shuffle=False)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 imput_size = 784
@@ -98,38 +176,14 @@ learning_rate = 0.001
 batch_size = 32
 num_epochs = 3
 
-entire_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
-
-train_ds, val_ds = random_split(entire_dataset, [55000, 5000])
-
-test_ds = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
-
-train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
 
 model = NN(input_size=imput_size, num_classes=num_classes).to(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+data_module = MnistDataModule(data_dir='dataset/', batch_size=batch_size, num_workers=4)
 
 trainer = pl.Trainer(accelerator='cpu', devices=1, min_epochs = 1 ,max_epochs=num_epochs, precision=16)
-trainer.fit(model, train_loader, val_loader)
+trainer.fit(model, data_module)
 
-trainer.validate(model, val_loader)
-trainer.test(model, test_loader)
-
-for epoch in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(train_loader):
-        data = data.to(device=device)
-        targets = targets.to(device=device)
-
-        data = data.reshape(data.shape[0], -1)
-
-        scores = model(data)
-        loss = criterion(scores, targets)
-
-        optimizer.zero_grad()
-        loss.backward()
-
-        optimizer.step()
+trainer.validate(model, data_module)
+trainer.test(model, data_module)
