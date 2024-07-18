96a6820847447b8332da2e95cd5926b32e341a6c
pranzalkhadka <pranzalkhadka1@gmail.com>
2024-07-18 15:51:57 +0545

    profiler

--- train.py
+++ train.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     model = NN(input_size = config.imput_size, num_classes = config.num_classes, learning_rate = config.learning_rate).to(config.device)
     data_module = MnistDataModule(data_dir = config.data_dir, batch_size = config.batch_size, num_workers = config.num_worker)
 
-    trainer = pl.Trainer(logger = logger,
+    trainer = pl.Trainer(profiler= "simple",
+                        logger = logger,
                         accelerator = config.accelerator, 
                         devices = config.devices, 
                         min_epochs = config.min_epochs, 
